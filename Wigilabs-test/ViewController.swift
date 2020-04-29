8fb891fddedf51aa55d8038f0a65a4a386e6b78e
Juan Camilo Navarro <jcinfonet.kshone@gmail.com>
2020-04-29 00:49:40 -0500

    Initial Commit

---
+++ Wigilabs-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wigilabs-test
+//
+//  Created by Jc on 29/04/20.
+//  Copyright © 2020 Jc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
