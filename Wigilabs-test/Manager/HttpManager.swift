0bf983076130470884148becde6a66fdadf6fc9a
Juan Camilo Navarro <jcinfonet.kshone@gmail.com>
2020-04-29 01:14:56 -0500

    first commit

---
+++ Wigilabs-test/Manager/HttpManager.swift
@@ -0,0 +1,15 @@
+//
+//  HttpManager.swift
+//  Wigilabs-test
+//
+//  Created by Jc on 29/04/20.
+//  Copyright © 2020 Jc. All rights reserved.
+//
+
+import Foundation
+
+class HttpManager {
+    func get() {
+        
+    }
+}
